6bbd829c8841e0db0aa58e1b09719e013afa7732
seymakara <seymakara88@gmail.com>
2019-03-25 15:58:21 -0700

    add roman to integer

---
+++ 13-romanToInteger.js
@@ -0,0 +1,15 @@
+var romanToInt = function (s) {
+  roman = { 'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1 }
+  num = 0
+
+  for (let i = 0; i < s.length; i++) {
+    if (roman[s[i] < roman[s[i + 1]]) {
+      num -= roman[s[i]];
+    } else {
+      num += roman[s[i]];
+    }
+  }
+  return num;
+
+
+};
