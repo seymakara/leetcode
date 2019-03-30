a2d82a3c3bb4f02c458d7cc8cdd9ed5332a379e8
seymakara <seymakara88@gmail.com>
2019-03-29 17:15:43 -0700

    add #45 solution

---
+++ 45-jumpGame2.js
@@ -0,0 +1,14 @@
+var jump = function (nums) {
+  let jumps = 0;
+  let curEnd = 0;
+  let curFarthest = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    curFarthest = Math.max(curFarthest, i + nums[i]);
+    if (i === curEnd) {
+      jumps++;
+      curEnd = curFarthest;
+      console.log('curEnd', curEnd)
+    }
+  }
+  return jumps;
+};
