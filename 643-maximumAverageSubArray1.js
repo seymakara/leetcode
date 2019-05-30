f2f71c0ec74e891a6803dad832b6c2b6cf8153ba
seymakara <seymakara88@gmail.com>
2019-05-29 19:02:10 -0700

    add 643

---
+++ 643-maximumAverageSubArray1.js
@@ -0,0 +1,15 @@
+const findMaxAverage = (nums, k) => {
+  let sum = 0;
+  let max = -Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+
+    if (i >= k - 1) {
+      max = Math.max(max, sum / k);
+      sum -= nums[i - k + 1];
+    }
+  }
+
+  return max;
+};
