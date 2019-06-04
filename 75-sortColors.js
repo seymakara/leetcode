e5bad62caf3bb1f96a28474eab12be737d28337c
seymakara <seymakara88@gmail.com>
2019-06-03 21:42:37 -0700

    add 75

---
+++ 75-sortColors.js
@@ -0,0 +1,18 @@
+var sortColors = function (nums) {
+  let low = 0;
+  let high = nums.length - 1;
+
+  for (let i = 0; i <= high; i++) {
+    if (nums[i] === 0) {
+      [nums[i], nums[low]] = [nums[low], nums[i]]
+      low++
+
+    }
+    else if (nums[i] === 2) {
+      [nums[i], nums[high]] = [nums[high], nums[i]]
+      high--;
+      i--;
+    }
+  }
+  return nums;
+};
